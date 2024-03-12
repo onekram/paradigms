da37b6fbfb5e4a360b6f9643a6fb9296696b6f51
Ivan Kramarenko <ivankramarenko13@gmail.com>
2024-03-12 12:25:27 +0300

    feat: HW 6 PieMinMax

--- javascript-solutions/functionalExpression.js
+++ javascript-solutions/functionalExpression.js
@@ -1,9 +1,17 @@
 'use strict'
 
-function getOperation(apply) {
-    let func =  (...v) => (x, y, z) => apply(...v.map((el) => el(x, y, z)));
-    func.count = apply.length;
+function getOperation(apply, count = apply.length) {
+    let func =  (...args) => (...v) => apply(...args.map((el) => el(...v)));
+    func.count = count;
     return func;
+}
+
+function cnst(value) {
+    return () => value;
+}
+
+function variable(name) {
+    return (...v) => v[variables[name]];
 }
 
 let add = getOperation((a, b) => a + b);
@@ -11,26 +19,34 @@ let subtract = getOperation((a, b) => a - b);
 let multiply = getOperation((a, b) => a * b);
 let divide = getOperation((a, b) => a / b);
 let negate = getOperation((a) => -a);
+let max3 = getOperation(Math.max, 3);
+let min5 = getOperation(Math.min, 5);
 
 
-function cnst(value) {
-    return (x, y, z) => value;
+let pi = cnst(Math.PI)
+let e = cnst(Math.E)
+
+
+let cnsts = {
+    'pi' : pi,
+    'e' : e
 }
 
-function variable(name) {
-    return (x, y, z) => {
-        switch (name) {
-            case 'x':
-                return x
-            case 'y':
-                return y
-            case 'z':
-                return z
-            default:
-                throw "Error: Unexpected variable name " + name
-        }
-    };
-}
+let variables = {
+    'x': 0,
+    'y': 1,
+    'z': 2,
+};
+
+let operations = {
+    '+': add,
+    '-': subtract,
+    '*': multiply,
+    '/': divide,
+    'negate': negate,
+    'min5': min5,
+    'max3' : max3
+};
 function test() {
     let expr = add(
         subtract(
@@ -47,41 +63,28 @@ function test() {
     );
 
     for (let x= 0; x < 11; x++) {
-        console.log(expr(x, 0, 0));
+        console.log(expr(x));
     }
 }
 
 function parse(expression) {
-    let operations = {
-        '+': add,
-        '-': subtract,
-        '*': multiply,
-        '/': divide,
-        'negate': negate
-    };
     let tokens = expression.trim().split(/\s+/)[Symbol.iterator]();
     let stack = [];
     let el;
     while (!(el = tokens.next()).done) {
         if (el.value in operations) {
-            let func = operations[el.value]
-            let count = func.count;
-            let arr = [];
-            for (let i = 0; i < count; i++) {
-                arr.unshift(stack.pop())
-            }
-            stack.push(func(...arr))
+            let func = operations[el.value];
+            stack.push(func(...stack.splice(-func.count)));
+        } else if (el.value in variables) {
+            stack.push(variable(el.value));
+        } else if (el.value in cnsts) {
+            stack.push(cnsts[el.value]);
+        } else if (!isNaN(Number(el.value))) {
+            stack.push(cnst(Number(el.value)));
         } else {
-            if (!isNaN(Number(el.value))) {
-                stack.push(cnst(Number(el.value)))
-            } else {
-                stack.push(variable(el.value))
-            }
+            throw 'Unexpected token ' + el.value;
         }
     }
     return stack.pop()
-
 }
-
-// test();
-console.log(parse("x x 2 - * x * 1 +")(5, 0, 0))
+// test()
