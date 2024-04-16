25b0093cf35c455d29f07b18abf56b4de4bbf492
Ivan Kramarenko <ivankramarenko13@gmail.com>
2024-04-16 13:48:23 +0300

    feat: HW 10 Means

--- clojure-solutions/expression.clj
+++ clojure-solutions/expression.clj
@@ -13,6 +13,16 @@
 (defn div
   ([x] (/ 1.0 x))
   ([x & args] (/ (double x) (apply * args))))
+(defn arith-mean-impl [& args]
+  (div (apply + args) (count args)))
+(defn geom-mean-impl [& args]
+  (Math/pow (abs (apply * args)) (div 1.0 (count args))))
+
+(defn harm-mean-impl [& args]
+  (double (div (count args) (apply + (map #(div 1 %) args)))))
+(def arithMean (get-op arith-mean-impl))
+(def geomMean (get-op geom-mean-impl))
+(def harmMean (get-op harm-mean-impl))
 
 (def add (get-op +))
 (def subtract (get-op -))
@@ -21,11 +31,14 @@
 (def negate subtract)
 
 (def func-map
-  {'+ add
-   '- subtract
-   '* multiply
-   '/ divide
-   'negate negate})
+  {'+         add
+   '-         subtract
+   '*         multiply
+   '/         divide
+   'negate    negate
+   'arithMean arithMean
+   'geomMean  geomMean
+   'harmMean harmMean})
 
 (defn parse-rec [arg]
   (cond (number? arg) (constant arg)
