042c97e60664dd433eff99ca67244fa2cb1a943e
Ivan Kramarenko <ivankramarenko13@gmail.com>
2024-04-16 14:00:22 +0300

    style: Fix space lines

--- clojure-solutions/expression.clj
+++ clojure-solutions/expression.clj
@@ -17,13 +17,12 @@
   (div (apply + args) (count args)))
 (defn geom-mean-impl [& args]
   (Math/pow (abs (apply * args)) (div 1.0 (count args))))
-
 (defn harm-mean-impl [& args]
   (double (div (count args) (apply + (map #(div 1 %) args)))))
+
 (def arithMean (get-op arith-mean-impl))
 (def geomMean (get-op geom-mean-impl))
 (def harmMean (get-op harm-mean-impl))
-
 (def add (get-op +))
 (def subtract (get-op -))
 (def divide (get-op div))
