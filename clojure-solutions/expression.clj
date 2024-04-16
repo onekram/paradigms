8e04d5d6ac7d158c2be56d4707d33c7aa04b82ad
Ivan Kramarenko <ivankramarenko13@gmail.com>
2024-04-16 07:31:17 +0300

    feat: HW 10 hard base

---
+++ clojure-solutions/expression.clj
@@ -0,0 +1,38 @@
+(defn get-op [f]
+  (fn [& args]
+    (fn [m]
+      (apply f (map #(% m) args)))))
+
+(defn constant [arg]
+  (constantly arg))
+
+(defn variable [arg]
+  (fn [m]
+    {:pre [(contains? m arg)]
+     :post [(number? %)]}
+    (m arg)))
+
+(defn div
+  ([x] (/ 1.0 x))
+  ([x & args] (/ (double x) (apply * args))))
+
+(def add (get-op +))
+(def subtract (get-op -))
+(def divide (get-op div))
+(def multiply (get-op *))
+(def negate subtract)
+
+(def func-map
+  {'+ add
+   '- subtract
+   '* multiply
+   '/ divide
+   'negate negate})
+
+(defn parse-rec [arg]
+  (cond (number? arg) (constant arg)
+        (symbol? arg) (variable (str arg))
+        (list? arg) (apply (func-map (first arg)) (map parse-rec (rest arg)))))
+
+(defn parseFunction [expr]
+  (parse-rec (read-string expr)))
