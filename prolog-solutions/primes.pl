e769fb9f22740f7da7a27e1492a1ebd4faf1d09b
Ivan Kramarenko <ivankramarenko13@gmail.com>
2024-05-19 13:14:12 +0300

    refactor: Delete useless rule

--- prolog-solutions/primes.pl
+++ prolog-solutions/primes.pl
@@ -16,12 +16,10 @@ next_divisor(N, F, R) :- F1 is F + 1, next_divisor(N, F1, R).
 prime_divisors(1, []) :- !.
 prime_divisors(N, [N]) :- prime(N), !.
 prime_divisors(N, FS) :- number(N), !, prime_divisors(N, 2, FS).
-prime_divisors(N, FS) :- check(N, FS).
+prime_divisors(R, [H1, H2 | T]) :-  H1 =< H2, prime(H1), prime_divisors(R1, [H2 | T]), R is R1 * H1.
 
 prime_divisors(N, _, [N]) :- prime(N), !.
 prime_divisors(N, F, [HFS | TFS]) :- next_divisor(N, F, HFS), N1 is div(N, HFS), prime_divisors(N1, HFS, TFS).
-
-check(R, [H1, H2 | T]) :- H1 =< H2, prime(H1), prime_divisors(R1, [H2 | T]), R is R1 * H1.
 
 intersection(_, [], []) :- !.
 intersection([], _, []) :- !. 
