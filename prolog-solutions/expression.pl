42afb925cdc66d88556f75ee556bfd0befd1d737
Ivan Kramarenko <ivankramarenko13@gmail.com>
2024-05-28 16:02:34 +0300

    refacotor: HW 15 ref

--- prolog-solutions/expression.pl
+++ prolog-solutions/expression.pl
@@ -10,7 +10,6 @@ to_lower(C, LC) :-
 lookup(K, [(K1, V) | _], V) :- atom_chars(K, [H | _]), to_lower(H, K1).
 lookup(K, [_ | T], V) :- lookup(K, T, V).
 
-
 ws --> [].
 ws --> [' '], ws.
 
@@ -21,44 +20,49 @@ check_p([H | T], L) -->
 
 unop_p(op_negate) --> [n, e, g, a, t, e].
 unop_p(op_bitnot) --> ['~'].
-op_p(op_add)    --> ['+'].
-op_p(op_subtract) --> ['-'].
-op_p(op_multiply) --> ['*'].
-op_p(op_divide)  --> ['/'].
-op_p(op_bitand)  --> ['&'].
-op_p(op_bitor)   --> ['|'].
-op_p(op_bitxor)  --> ['^'].
-terop_p(op_bitif) --> ['?'].
-terop_p(op_bitmux) --> ['¿'].
 
-expr_p(variable(Name)) -->
- { nonvar(Name) -> call(atom_chars(Name, Chars)); var(Name) },
- ws, check_p(Chars, ['X', 'Y', 'Z', x, y, z]), ws,
- {Chars = [_ | _], atom_chars(Name, Chars)}.
+binop_p(op_add)    --> ['+'].
+binop_p(op_subtract) --> ['-'].
+binop_p(op_multiply) --> ['*'].
+binop_p(op_divide)  --> ['/'].
+binop_p(op_bitand)  --> ['&'].
+binop_p(op_bitor)   --> ['|'].
+binop_p(op_bitxor)  --> ['^'].
+
+ternop_p(op_bitif) --> ['?'].
+ternop_p(op_bitmux) --> ['¿'].
+
+expr_p(variable(N)) -->
+ { nonvar(N) -> call(atom_chars(N, CS)); var(N) },
+ ws, check_p(CS, ['X', 'Y', 'Z', x, y, z]), ws,
+ {CS = [_ | _], atom_chars(N, CS)}.
 
 
-expr_p(const(Value)) -->
-  { nonvar(Value) -> call(number_chars(Value, Chars)); var(Value) },
-  ws, check_p(Chars, ['.', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-']), ws,
-  { Chars = [_ | _], 
-  \= (Chars, ['-']), 
-  number_chars(Value, Chars) }.
+expr_p(const(V)) -->
+  { nonvar(V) -> call(number_chars(V, CS)); var(V) },
+  ws, check_p(CS, ['.', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-']), ws,
+  { CS = [_ | _], CS \= ['-'], number_chars(V, CS) }.
 
-  
-expr_p(operation(Op, A)) --> 
-		ws, unop_p(Op), 
-		[' '], ws, expr_p(A), ws.
-		
-expr_p(operation(Op, A, B)) --> 
-		ws, ['('], ws, expr_p(A), 
-		[' '], ws, op_p(Op), [' '],
-		ws, expr_p(B), ws, [')'], ws.
-		
-expr_p(operation(Op, C, T, F)) --> 
-		ws, ['('], ws, expr_p(C), [' '],
-		ws, terop_p(Op), [' '],  ws, 
-		expr_p(T), [' '], ws, 
-		[':'], [' '], ws, expr_p(F), ws, [')'], ws.
+expr_p(operation(OP, A)) -->
+    ws, unop_p(OP),
+    [' '], ws,
+    expr_p(A), ws.
+
+expr_p(operation(OP, A, B)) -->
+    ws, ['('],
+    ws, expr_p(A), [' '],
+    ws, binop_p(OP), ws,
+    [' '], expr_p(B),
+    ws, [')'], ws.
+
+expr_p(operation(OP, A, B, C)) -->
+    ws, ['('],
+    ws, expr_p(A), [' '],
+    ws, ternop_p(OP), ws,
+    [' '], expr_p(B),
+    ws, [' '], [':'], [' '], ws,
+    expr_p(C), ws,
+    [')'], ws.
 
 infix_str(E, A) :- ground(E),
     phrase(expr_p(E), C), atom_chars(A, C), !.
@@ -66,9 +70,18 @@ infix_str(E, A) :- ground(E),
 infix_str(E, A) :- atom(A),
     atom_chars(A, C), phrase(expr_p(E), C), !.
 
-evaluate(const(Value), _, Value).
-evaluate(variable(Name), VS, V) :-
-    lookup(Name, VS, V).
+evaluate(const(V), _, V).
+evaluate(variable(N), VS, V) :-
+    lookup(N, VS, V).
+
+evaluate(operation(OP, A), VS, R) :-
+    evaluate(A, VS, RA),
+    op_impl(OP, RA, R).
+
+evaluate(operation(OP, A, B), VS, R) :-
+    evaluate(A, VS, RA),
+    evaluate(B, VS, RB),
+    op_impl(OP, RA, RB, R).
 
 evaluate(operation(OP, A, B, C), VS, R) :-
     evaluate(A, VS, RA),
@@ -76,17 +89,9 @@ evaluate(operation(OP, A, B, C), VS, R) :-
     evaluate(C, VS, RC),
     op_impl(OP, RA, RB, RC, R).
 
-evaluate(operation(OP, A, B), VS, R) :-
-    evaluate(A, VS, RA),
-    evaluate(B, VS, RB),
-    op_impl(OP, RA, RB, R).
+op_impl(op_negate, A, R) :- R is -A.
+op_impl(op_bitnot, A, R) :- R is \A.
 
-evaluate(operation(OP, A), VS, R) :-
-    evaluate(A, VS, RA),
-    op_impl(OP, RA, R).
-
-op_impl(op_bitif, A, B, C, R) :- A /\ 1 =:= 1 -> R = B; R = C.
-op_impl(op_bitmux, A, B, C, R) :- R is (\A /\ B) \/ (A /\ C).
 op_impl(op_add, A, B, R) :- R is A + B.
 op_impl(op_subtract, A, B, R) :- R is A - B.
 op_impl(op_multiply, A, B, R) :- R is A * B.
@@ -95,5 +100,5 @@ op_impl(op_bitand, A, B, R) :- R is A /\ B.
 op_impl(op_bitor, A, B, R) :- R is A \/ B.
 op_impl(op_bitxor, A, B, R) :- R is (A \/ B) /\ \(A /\ B).
 
-op_impl(op_negate, A, R) :- R is -A.
-op_impl(op_bitnot, A, R) :- R is \A.
+op_impl(op_bitif, A, B, C, R) :- A /\ 1 =:= 1 -> R = B; R = C.
+op_impl(op_bitmux, A, B, C, R) :- R is (\A /\ B) \/ (A /\ C).
